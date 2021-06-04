7993242d3583f4630a8bc4ae9ea19782a1511271
Nikita PB <npb.nikita@gmail.com>
2021-06-04 20:38:24 +0300

    Adds basic main page components

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import MainScreen from '../main-screen/main-screen.jsx';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({ ads }) {
+  return (
+    <MainScreen ads={ads} />
+  );
 }
+
+App.propTypes = {
+  ads: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  ADS: [{}, {}, {}, {}, {}],
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      ads={Setting.ADS}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
